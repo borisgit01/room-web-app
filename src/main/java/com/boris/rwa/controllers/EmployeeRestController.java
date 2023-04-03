2e9aad70f19ae75c2584e7d40dce5b3bdf14c87b
borisgit01 <borisgodunovph@gmail.com>
2023-04-03 13:46:29 -0400

    Saving work in progress

---
+++ src/main/java/com/boris/rwa/controllers/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package com.boris.rwa.controllers;
+
+import com.boris.rwa.models.Employee;
+import com.boris.rwa.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeRestController {
+    private final EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ src/main/java/com/boris/rwa/controllers/RoomRestController.java
@@ -0,0 +1,24 @@
+package com.boris.rwa.controllers;
+
+import com.boris.rwa.models.Room;
+import com.boris.rwa.service.RoomService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomRestController {
+    private final RoomService roomService;
+
+    public RoomRestController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomService.getAllRoome();
+    }
+}
